8faed295e2cf844bb613e67531607bf51d6387df
Joel Wood <joelmwood@live.com>
2023-01-19 15:21:36 -0600

    Added additional box collider on player to prevent double jumps

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -14,14 +14,16 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D myRigidbody;
     Animator myAnimator;
 
-    CapsuleCollider2D myCapsuleCollider;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
         gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
@@ -39,7 +41,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void OnJump(InputValue value){
-        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
             return;
         }
         if(value.isPressed){
@@ -67,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
       void ClimbLadder(){
-        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))){ 
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))){ 
             myRigidbody.gravityScale = gravityScaleAtStart;
             return; 
         }
