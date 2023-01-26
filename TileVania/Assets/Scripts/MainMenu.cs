807bd2c2d2da2a46754124212c4c13ed1d82dde2
Joel Wood <joelmwood@live.com>
2023-01-26 12:56:27 -0600

    Added Basic Start Menu

---
+++ TileVania/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+        Debug.Log("Play");
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+
+
+    }
+    public void QuitGame(){
+        Debug.Log("Quit");
+    }
+
+    
+}
